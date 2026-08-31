ace8a42982860ba0f5f9843ccd03e94d2facd36e
Yamatatsu353011 <88118595+Yamatatsu353011@users.noreply.github.com>
2026-08-31 12:41:48 +0900

    Update mesh_daemon.py

--- mesh_daemon.py
+++ mesh_daemon.py
@@ -73,6 +73,9 @@ def main():
             
             if not isinstance(line, str):
                 continue
+
+            if line.startswith("TESTPING,") or line.startswith("TESTPONG,"):
+                continue
             
             packet = Packet.decode(line)
             
