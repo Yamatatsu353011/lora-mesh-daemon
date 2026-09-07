76c212655bd4ccbf8f60c0e8c51f94f67d32d86a
Yamatatsu353011 <88118595+Yamatatsu353011@users.noreply.github.com>
2026-09-07 17:37:32 +0900

    Update routing.py

--- routing.py
+++ routing.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import redis
-import time
+
 import config
 from mesh import DuplicateSuppressor
 from packet import Packet, make_reply_packet
@@ -93,8 +93,6 @@ class Routing:
     
             reply_line = reply_packet.encode()
 
-            time.sleep(random.uniform(0.1, 0.5))
-    
             self.redis.lpush(
                 config.REDIS_RAW_TX,
                 reply_line,
